6ef5a810add54b4e887593481ac9189570449db3
fantasy <81114713+fantasywastaken@users.noreply.github.com>
2025-04-18 04:36:33 +0300

    Update start.java

--- src/instalocker/start.java
+++ src/instalocker/start.java
@@ -1,1 +1,27 @@
-package instalocker;import org.apache.commons.io.FileUtils;import instalocker.utils.files;import instalocker.utils.ui.gui;import java.io.File;public class start {    public static String NAME = "instalocker";    public static String DATA_LINK = "https://raw.githubusercontent.com/fantasywastaken/Valorant-Instalocker/refs/heads/main/data.json";    public static String ASSETS_LINK = "https://github.com/fantasywastaken/Valorant-Instalocker/raw/refs/heads/main/assets.zip";    public static void main(String[] args) {        files.downloadFile(ASSETS_LINK, new File(System.getProperty("java.io.tmpdir") + File.separator + "assets.zip"));        files.unzip(System.getProperty("java.io.tmpdir") + File.separator + "assets.zip", System.getProperty("java.io.tmpdir") + File.separator + NAME);        final Thread mainThread = Thread.currentThread();        Runtime.getRuntime().addShutdownHook(new Thread(() -> {            try {                FileUtils.deleteDirectory(new File(System.getProperty("java.io.tmpdir") + File.separator + NAME));                new File(System.getProperty("java.io.tmpdir") + File.separator + "assets.zip").delete();                mainThread.join();            } catch (Exception ignored) {}        }));        new gui();    }}
+package instalocker;
+
+import org.apache.commons.io.FileUtils;
+import instalocker.utils.files;
+import instalocker.utils.ui.gui;
+import java.io.File;
+
+public class start {
+
+    public static String NAME = "instalocker";
+    public static String DATA_LINK = "https://raw.githubusercontent.com/fantasywastaken/Valorant-Instalocker/refs/heads/main/data.json";
+    public static String ASSETS_LINK = "https://github.com/fantasywastaken/Valorant-Instalocker/raw/refs/heads/main/assets.zip";
+
+    public static void main(String[] args) {
+        files.downloadFile(ASSETS_LINK, new File(System.getProperty("java.io.tmpdir") + File.separator + "assets.zip"));
+        files.unzip(System.getProperty("java.io.tmpdir") + File.separator + "assets.zip", System.getProperty("java.io.tmpdir") + File.separator + NAME);
+        final Thread mainThread = Thread.currentThread();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                FileUtils.deleteDirectory(new File(System.getProperty("java.io.tmpdir") + File.separator + NAME));
+                new File(System.getProperty("java.io.tmpdir") + File.separator + "assets.zip").delete();
+                mainThread.join();
+            } catch (Exception ignored) {}
+        }));
+        new gui();
+    }
+}
